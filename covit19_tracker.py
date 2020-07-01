17ea7fe3d29654b21ecfa1b47608ae49ac340813
Alison Orellana <ali15.orellan@gmail.com>
2020-07-01 16:10:22 -0400

    to_enter_a_country

--- covit19_tracker.py
+++ covit19_tracker.py
@@ -1,14 +1,13 @@
 from covid import Covid
 
-
 cov = Covid()
 data = cov.get_data()
 
-country = cov.get_status_by_country_name("Bolivia")
+specific_country = input("Enter a country: ")
+country = cov.get_status_by_country_name(specific_country)
 print(country)
 
 countries = cov.list_countries()
 
 active = cov.get_total_active_cases()
-print("Total active cases: ",active)
-
+print("Global active cases: ",active)
